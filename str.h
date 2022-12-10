5461747e3318402752154e51876b670dcb499389
CodeMouse179 <codemouse179@gmail.com>
2022-12-10 19:56:21 +0300

    add str.c, update str.h

--- str.h
+++ str.h
@@ -1,4 +1,18 @@
-﻿#ifndef CODEMOUSE_STR_H
+﻿//     +--------------------------------------------------------------------------------+
+//     |                                      str                                       |
+//     |  Introduction : Better string converter and utils for C11                      |
+//     |  Modified Date : 2022/12/10                                                    |
+//     |  License : MIT                                                                 |
+//     |  Source Code : https://github.com/CodeMouse179/str                             |
+//     |  Readme : https://github.com/CodeMouse179/str/blob/main/README.md              |
+//     |                                                                                |
+//     |                             Designer : CodeMouse179                            |
+//     |  Email : codemouse179@gmail.com                                                |
+//     |  Github : https://github.com/CodeMouse179                                      |
+//     |  Bilibili : https://space.bilibili.com/3461577785215838                        |
+//     +--------------------------------------------------------------------------------+
+
+#ifndef CODEMOUSE_STR_H
 #define CODEMOUSE_STR_H
 
 #endif
